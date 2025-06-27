0d8ffe912992ead11266922d536ab17bfbb30776
spydyy <shubhamvanzara04@gmail.com>
2025-06-27 12:05:14 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ const sessionOptions = {
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, Server");
-// });
+app.get("/", (req, res) => {
+    res.redirect('/listings')
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
